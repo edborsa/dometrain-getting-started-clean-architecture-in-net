33535da2baf406532cc863308a003a2531e8215f
Eduardo Borsa <eborsa@loomissayles.com>
2024-08-12 07:50:08 -0300

    IDK
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -1,5 +1,6 @@
-using GymManagement.Application.Services;
+using GymManagement.Application.Subscriptions.Commands.CreateSubscription;
 using GymManagement.Contracts.Subscriptions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagement.Api.Controllers;
@@ -8,20 +9,22 @@ namespace GymManagement.Api.Controllers;
 [Route("[controller]")]
 public class SubscriptionsController : ControllerBase
 {
-    private readonly ISubscriptionsWriteService _subscriptionsWriteService;
+    private readonly ISender _mediator;
 
-    public SubscriptionsController(ISubscriptionsWriteService subscriptionsWriteService)
+    public SubscriptionsController(ISender mediator)
     {
-        _subscriptionsWriteService = subscriptionsWriteService;
+        _mediator = mediator;
     }
 
     [HttpPost]
-    public IActionResult CreateSubscription([FromBody] CreateSubscriptionRequest request)
+    public async Task<IActionResult> CreateSubscription([FromBody] CreateSubscriptionRequest request)
     {
-        var subscriptionId =
-            _subscriptionsWriteService.CreateSubscription(request.SubscriptionType.ToString(), request.AdminId);
-        var response = new SubscriptionResponse(subscriptionId, request.SubscriptionType);
+        var command = new CreateSubscriptionCommand(request.SubscriptionType.ToString(), request.AdminId);
+        var createSubscriptionResult = await _mediator.Send(command);
 
-        return Ok(response);
+        return createSubscriptionResult.MatchFirst(
+            subscription => Ok(new SubscriptionResponse(subscription.Id, request.SubscriptionType)),
+            error => Problem()
+        );
     }
 }
