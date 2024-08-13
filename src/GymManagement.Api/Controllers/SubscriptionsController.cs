f4dc5ed9a215fdf30341a5ed3e6d98cdc5c70e2a
Eduardo Borsa <eborsa@loomissayles.com>
2024-08-13 10:35:46 -0300

    Stopped at Implementing the Repository Patter Using EF Core in the Infrastructure Layer
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -1,5 +1,7 @@
-using GymManagement.Application.Services;
+using GymManagement.Application.Subscriptions.Commands.CreateSubscription;
+using GymManagement.Application.Subscriptions.Queries;
 using GymManagement.Contracts.Subscriptions;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagement.Api.Controllers;
@@ -8,19 +10,32 @@ namespace GymManagement.Api.Controllers;
 [Route("[controller]")]
 public class SubscriptionsController : ControllerBase
 {
-    private readonly ISubscriptionsService _subscriptionsService;
+    private readonly IMediator _mediator;
 
-    public SubscriptionsController(ISubscriptionsService subscriptionsService)
+    public SubscriptionsController(IMediator mediator)
     {
-        _subscriptionsService = subscriptionsService;
+        _mediator = mediator;
     }
 
     [HttpPost]
-    public IActionResult CreateSubscription([FromBody] CreateSubscriptionRequest request)
+    public async Task<IActionResult> CreateSubscription([FromBody] CreateSubscriptionRequest request)
     {
-        var subscriptonId =
-            _subscriptionsService.CreateSubscription(request.SubscriptionType.ToString(), request.AdminId);
-        var response = new CreateSubscriptionResponse(subscriptonId, request.SubscriptionType);
-        return Ok(response);
+        var command = new CreateSubscriptionCommand(request.SubscriptionType.ToString(), request.AdminId);
+        var createSubscriptionResult = await _mediator.Send(command);
+        return createSubscriptionResult.Match(
+            subscription => Ok(new CreateSubscriptionResponse(subscription.Id, request.SubscriptionType)),
+            error => Problem()
+        );
+    }
+
+    [HttpGet("{subscriptionId:guid}")]
+    public async Task<IActionResult> GetSubscription(Guid subscriptionId)
+    {
+        var query = new GetSubscriptionQuery(subscriptionId);
+        var getSubscriptionResult = await _mediator.Send(query);
+        return getSubscriptionResult.Match(
+            subscription => Ok(new SubscriptionResponse(subscription.Id, Enum.Parse<SubscriptionType>(subscription.SubscriptionType))),
+            error => Problem()
+        );
     }
 }
